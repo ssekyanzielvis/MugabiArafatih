4c7a4073f29d0f48fa37000fa46ad2a8402fa268
ssekyanzi elvis <abdulsalamssekyanzi@gmail.com>
2026-01-02 15:30:42 +0300

    second commit

--- professional-portfolio/src/app/layout.tsx
+++ professional-portfolio/src/app/layout.tsx
@@ -29,7 +29,11 @@ export const metadata: Metadata = {
     description: 'Showcasing expertise and collaboration opportunities',
     keywords: ['portfolio', 'professional', 'developer', 'collaboration'],
     authors: [{ name: 'Mugabi Arafatih' }],
-    viewport: 'width=device-width, initial-scale=1',
+}
+
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
 }
 
 export default function RootLayout({
